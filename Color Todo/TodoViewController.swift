6e6664c112d42f11ba8081bad3e79c38ba956056
Daniel Kilders Diaz <dankil.dev@gmail.com>
2018-11-28 14:58:57 +0000

    added tableview with todo tasks that can be checkmarked and unchecked

--- Color Todo/TodoViewController.swift
+++ Color Todo/TodoViewController.swift
@@ -17,6 +17,33 @@ class TodoViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK: - Tableview datasource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - Tableview delegates
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let currentCell = tableView.cellForRow(at: indexPath)
+        
+        if currentCell?.accessoryType == .checkmark {
+            currentCell?.accessoryType = .none
+        } else {
+            currentCell?.accessoryType = .checkmark
+        }
+    }
 
 }
 
